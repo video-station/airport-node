72c9f68a983b7cd2850f417fb446a81af4140f00
Akash9773312 <am9773312@outlook.com>
2023-09-25 23:21:36 +0530

    base code for bluetooth data transfer to redis key

---
+++ blu_data_send.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+import redis
+
+rdev = redis.Redis(
+    host="127.0.0.1",
+    port=6379,
+    password=""
+)
+
+excel_file = 'data\\media.xlsx'  
+df = pd.read_excel(excel_file)
+data = df.to_dict(orient='records')
+
+json_data = json.dumps(data)
+
+try:
+    rdev.set("content", json_data)
+    print(f"JSON data sent to Redis key 'content' successfully.")
+except Exception as e:
+    print(f"Failed to send data to Redis: {str(e)}")
