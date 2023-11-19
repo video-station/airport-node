3288b353ef5dc70ff37260c8f53bcb31172a97c8
Akash9773312 <am9773312@outlook.com>
2023-11-19 16:05:13 +0530

    added switch script and switch in starter

--- starter.sh
+++ starter.sh
@@ -4,3 +4,9 @@ pkill -f bt-obex &
 
 su - sotpurk -c "/usr/bin/bash /home/sotpurk/airport-node/bluerecv.sh" &
 #su - sotpurk -c "/usr/bin/bash /home/sotpurk/airport-node/blueagent.sh" &
+sleep 3
+while true; do
+tput setaf 3; echo "Starting Switcher" ; tput sgr0
+sleep 3
+python3 -u /home/sotpurk/airport-node/switch.py #2>&1 | tee -a /logs/netman.log
+done &
---
+++ switch.py
@@ -0,0 +1,36 @@
+import redis 
+import subprocess
+import time
+
+rdev = redis.Redis(
+    host="127.0.0.1",
+    port="6379",
+    password="",
+    decode_responses=True
+)
+
+previous_state = None
+
+while True:
+    try:
+        current_state = rdev.get("switch")
+            
+        if current_state != previous_state:
+            print(f"Switch state changed: {previous_state} to {current_state}")
+
+            if current_state == "on":
+                print("Switch is on................................................................")
+                subprocess.run("video -k && chvt 7 &", shell=True)
+                # subprocess.run("su - sotpurk -c 'export DISPLAY=:0 && cd /home/sotpurk/airport-node/ && node index.js' &", shell=True)
+                subprocess.run("su - sotpurk -c 'export DISPLAY=:0 && /bin/bash /home/sotpurk/airport-node/startindex.sh'" , shell=True)
+
+            else:
+                print("Switch is off................................................................................")
+                subprocess.run("pkill -f chromium | exit 0", shell=True)
+                subprocess.run("chvt 1 && video -s", shell=True)
+            previous_state = current_state
+
+        time.sleep(1)  
+
+    except Exception as e:
+        print("Error:", e)
